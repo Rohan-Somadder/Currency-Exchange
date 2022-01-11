f7cc9ff667ba44353f2dec1689ec572cbf763a1a
Rohan Somadder <rohansomadder10@gmail.com>
2022-01-11 22:11:17 +0530

    Improved UI

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 '''
 Basic Currency Exchange Application based on ExchangeRate-Api
 
-# Todo: complete the currency.compare() function
-# Todo: implement the currency.compare() function
+# Todo: None
 '''
 
 import requests
@@ -17,7 +16,6 @@ class CurrencyExchange():
     def __init__(self, url):
         self.data = requests.get(url).json()
         self.rates = self.data['conversion_rates']
-        self.currencies = self.data['conversion_rates'].keys()
 
     def convert(self, to_currency, amount):
         # limiting the precision to 4 decimal places
@@ -27,20 +25,22 @@ class CurrencyExchange():
     def compare(self, first_currency, second_currency):
         if self.rates[first_currency] < self.rates[second_currency]:
             print(f"\n{first_currency} is more valuable than {second_currency}")
-            print(f"1 {first_currency} = {self.rates[second_currency]/self.rates[first_currency]} {second_currency}\n")
+            print(
+                f"1 {first_currency} = {self.rates[second_currency]/self.rates[first_currency]} {second_currency}\n")
         elif self.rates[first_currency] > self.rates[second_currency]:
             print(f"\n{second_currency} is more valuable than {first_currency}")
-            print(f"1 {second_currency} = {self.rates[first_currency]/self.rates[second_currency]} {first_currency}\n")
+            print(
+                f"1 {second_currency} = {self.rates[first_currency]/self.rates[second_currency]} {first_currency}\n")
         else:
             print("\nBoth are of same value.\n")
-
 
 
 def exchange():
     ''' Function for exchange of the currencies'''
     currency_code = input("\nEnter the currency code: ")
     if currency_code not in CURR:
-        print(f"\n{currency_code} is not supported, changing the base currency to USD...")
+        print(
+            f"\n{currency_code} is not supported, changing the base currency to USD...")
         currency_code = "USD"
 
     url = LINK + API_KEY + "/latest/" + currency_code
@@ -60,7 +60,7 @@ def exchange():
         print("\nThe input amount you entered is incorrect, input amount set to 1...")
         amount = 1
 
-    print(f"{amount} {currency_code} = {currency.convert(exchange_code,amount)} {exchange_code}")
+    print(f"{amount} {currency_code} = {currency.convert(exchange_code,amount)} {exchange_code}\n")
 
 
 def compare():
@@ -83,21 +83,24 @@ def compare():
 
     currency.compare(curr_1, curr_2)
 
+
 def main():
     ''' Handles the greetings and initial displays'''
-    print('CURRENCY EXCHANGE APP'.center(51, '='), "\n\n")
-    print('MENU'.center(51, '-'), '\n')
-    print('1.     Exchange currency')
-    print('2.     Comapre Currencies')
-    print('Else.  Exit')
-    try:
-        choice = int(input('Enter :  '))
-    except ValueError:
-        choice = -1
-    if choice == 1:
-        exchange()
-    if choice == 2:
-        compare()
+    choice = 1
+    while choice in [1, 2]:
+        print('CURRENCY EXCHANGE APP'.center(51, '='), "\n\n")
+        print('MENU'.center(51, '-'), '\n')
+        print('1.     Exchange currency')
+        print('2.     Comapre Currencies')
+        print('Else.  Exit')
+        try:
+            choice = int(input('Enter :  '))
+        except ValueError:
+            choice = -1
+        if choice == 1:
+            exchange()
+        if choice == 2:
+            compare()
 
 
 if __name__ == '__main__':
